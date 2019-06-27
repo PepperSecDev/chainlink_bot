96ec3d9bd6f05dc6df8d5974c3813d52fd39dd86
Roman Storm <rstormsf@gmail.com>
2019-06-26 21:49:22 -0700

    add

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 require('dotenv').config()
 const ABI = require('./consumer.abi.json')
-const Web3 = require('web3')
 let { CONSUMER_CONTRACT, REQUEST_INTERVAL, RPC_URL, PRIVATE_KEY, NODE_JOB_ID, ORACLE_CONTRACT } = process.env
-const web3 = new Web3(RPC_URL)
 const ethers = require('ethers')
 
 async function main() {
-    // web3.eth.accounts.wallet.add('0x' + PRIVATE_KEY)
-    // const consumer = new web3.eth.Contract(ABI, CONSUMER_CONTRACT)
-    // console.log('before send')
-    // const tx = consumer.methods.requestEthereumPrice(ORACLE_CONTRACT, NODE_JOB_ID).send({
-    //     from: web3.eth.accounts.wallet.accounts[0].address,
-    //     gasPrice: '1000000000',
-    //     gas: '200000'
-    // })
-
-    // console.log('tx', tx)
-    // exit(0)
     let provider = ethers.getDefaultProvider('ropsten');
     let wallet = new ethers.Wallet('0x' + PRIVATE_KEY, provider);
     let contract = new ethers.Contract(CONSUMER_CONTRACT, ABI, provider);
