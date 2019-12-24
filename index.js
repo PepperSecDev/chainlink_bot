c3ed550f6eb1390313747bbd83d190cd827de178
Roman Storm <rstormsf@gmail.com>
2019-12-23 20:03:04 -0800

    btc example

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ let { CONSUMER_CONTRACT, REQUEST_INTERVAL, RPC_URL, PRIVATE_KEY, NODE_JOB_ID, OR
 const ethers = require('ethers')
 
 async function main() {
-    let provider = ethers.getDefaultProvider('ropsten');
-    let wallet = new ethers.Wallet('0x' + PRIVATE_KEY, provider);
-    let contract = new ethers.Contract(CONSUMER_CONTRACT, ABI, provider);
-    let contractWithSigner = contract.connect(wallet);
-
-    let tx = await contractWithSigner.requestEthereumPrice(ORACLE_CONTRACT, NODE_JOB_ID, {
-        gasPrice: ethers.utils.parseUnits('1.0', 'gwei'),
-    });
-    console.log(tx.hash)
-    await tx.wait();
-    setTimeout(main, 1000 * 300)
-    console.log('waiting 3600 sec', Date.now())
+    try {
+        let provider = ethers.getDefaultProvider('rinkeby');
+        let wallet = new ethers.Wallet('0x' + PRIVATE_KEY, provider);
+        let contract = new ethers.Contract(CONSUMER_CONTRACT, ABI, provider);
+        let contractWithSigner = contract.connect(wallet);
+    
+        let tx = await contractWithSigner.requestBTCPrice(ORACLE_CONTRACT, NODE_JOB_ID, {
+            gasPrice: ethers.utils.parseUnits('2.0', 'gwei'),
+        });
+        console.log(tx.hash)
+        await tx.wait();
+        setTimeout(main, 1000 * 300)
+        console.log('waiting 300 seconds', Date.now())
+    } catch(e) {
+        console.log(e)
+    }
 }
 
 main()
