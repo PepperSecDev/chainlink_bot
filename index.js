0669f7f7f5d634915a3fd707b1fda1bc8b3e4e8b
Roman Storm <roman@peppersec.com>
2020-03-17 23:52:39 -0700

    update

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-require('dotenv').config()
-const ABI = require('./consumer.abi.json')
-let { CONSUMER_CONTRACT, REQUEST_INTERVAL, RPC_URL, PRIVATE_KEY, NODE_JOB_ID, ORACLE_CONTRACT } = process.env
-const ethers = require('ethers')
+require("dotenv").config({ path: ".env" });
+const ABI = require("./consumer.abi.json");
+let {
+  CONSUMER_CONTRACT,
+  REQUEST_INTERVAL,
+  PRIVATE_KEY,
+  NODE_JOB_ID,
+  ORACLE_CONTRACT,
+  NETWORK,
+  GAS_PRICE
+} = process.env;
+const ethers = require("ethers");
 
 async function main() {
-    let provider = ethers.getDefaultProvider('ropsten');
-    let wallet = new ethers.Wallet('0x' + PRIVATE_KEY, provider);
-    let contract = new ethers.Contract(CONSUMER_CONTRACT, ABI, provider);
-    let contractWithSigner = contract.connect(wallet);
+  let provider = ethers.getDefaultProvider(NETWORK);
+  let wallet = new ethers.Wallet("0x" + PRIVATE_KEY, provider);
+  let contract = new ethers.Contract(CONSUMER_CONTRACT, ABI, provider);
+  let contractWithSigner = contract.connect(wallet);
 
-    let tx = await contractWithSigner.requestEthereumPrice(ORACLE_CONTRACT, NODE_JOB_ID, {
-        gasPrice: ethers.utils.parseUnits('3.0', 'gwei'),
-    });
-    console.log(tx.hash)
-    await tx.wait();
-    setTimeout(main, 1000 * Number(REQUEST_INTERVAL) * 60)
-    console.log(`waiting ${Number(REQUEST_INTERVAL)} min`, Date.now())
+  let tx = await contractWithSigner.requestEthereumPrice(
+    ORACLE_CONTRACT,
+    NODE_JOB_ID,
+    {
+      gasPrice: ethers.utils.parseUnits(GAS_PRICE, "gwei")
+    }
+  );
+  console.log(tx.hash);
+  await tx.wait();
+  setTimeout(main, 1000 * Number(REQUEST_INTERVAL) * 60);
+  console.log(`waiting ${Number(REQUEST_INTERVAL)} min`, Date.now());
 }
 
-main()
+main();
